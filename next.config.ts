525b07ce05e3d748f10ec79013a7efd1b270f30f
Vinay Tiwari <109553738+vinaytz@users.noreply.github.com>
2025-07-12 23:11:13 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.instagram.com',
+        port: '',
+        pathname: '/static/images/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
